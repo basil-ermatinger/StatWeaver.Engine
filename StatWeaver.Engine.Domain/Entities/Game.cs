95f450d7711ea6ab224ac85abc820348730ac35e
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2026-02-27 10:17:08 +0100

    Add project files.

---
+++ StatWeaver.Engine.Domain/Entities/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace StatWeaver.Engine.Domain.Entities;
+
+public class Game
+{
+	public int Id { get; set; }
+
+	public Guid Uid { get; set; }
+
+	public string Identifikation { get; set; }
+
+	public string Name { get; set; }
+
+	public bool IsActive { get; set; }
+
+	public DateTime _CreatedAt { get; set; }
+
+	public DateTime _ModifiedAt { get; set; }
+}
