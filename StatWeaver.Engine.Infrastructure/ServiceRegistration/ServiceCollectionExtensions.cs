fcf07635a8f317c980cea7253a8344310223659d
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2026-03-01 10:59:36 +0100

    Added connection string to db as well as initializing of the service and a first migration

---
+++ StatWeaver.Engine.Infrastructure/ServiceRegistration/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using StatWeaver.Engine.Infrastructure.DbContexts;
+
+namespace StatWeaver.Engine.Infrastructure.ServiceRegistration;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+	{
+		string? connectionString = configuration.GetConnectionString("StatWeaverDbConnectionString");
+		services.AddDbContext<StatWeaverDbContext>(options => options.UseSqlServer(connectionString));
+		return services;
+	}
+}
