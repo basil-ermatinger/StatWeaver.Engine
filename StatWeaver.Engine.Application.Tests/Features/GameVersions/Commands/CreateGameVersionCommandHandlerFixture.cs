118ba077ffdf50cb1efe5054beb66f01c82a2fce
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2026-03-13 20:23:58 +0100

    #10 Setup MSTest

---
+++ StatWeaver.Engine.Application.Tests/Features/GameVersions/Commands/CreateGameVersionCommandHandlerFixture.cs
@@ -0,0 +1,16 @@
+﻿namespace StatWeaver.Engine.Application.Tests.Features.GameVersions.Commands;
+
+[TestClass]
+public class CreateGameVersionCommandHandlerFixture
+{
+	[TestMethod]
+	public void Handle_CreateNewGameVersion_GameVersionCreated()
+	{
+		// Arrange
+
+		// Act
+
+		// Assert
+		Assert.IsTrue(true);
+	}
+}
